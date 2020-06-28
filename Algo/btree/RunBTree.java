c78b17c1dfd323e754d52b4c1e5b54bf8da0895b
JohnVX <wswbpf@163.com>
2020-06-28 18:01:42 +0800

    测试用例

--- Algo/btree/RunBTree.java
+++ Algo/btree/RunBTree.java
@@ -8,10 +8,19 @@ public class RunBTree {
         btree.insertEntry(new BTreeNode.Entry<>(30, "john"));
         btree.insertEntry(new BTreeNode.Entry<>(40, "sara"));
         btree.insertEntry(new BTreeNode.Entry<>(50, "reese"));
-        btree.insertEntry(new BTreeNode.Entry<>(11, "trump"));
-        btree.insertEntry(new BTreeNode.Entry<>(12, "holy"));
-        btree.insertEntry(new BTreeNode.Entry<>(13, "jack"));
-        btree.deleteEntryByKey(11);
+        btree.insertEntry(new BTreeNode.Entry<>(60, "trump"));
+        btree.insertEntry(new BTreeNode.Entry<>(70, "70"));
+        btree.insertEntry(new BTreeNode.Entry<>(80, "80"));
+        btree.insertEntry(new BTreeNode.Entry<>(90, "90"));
+        btree.insertEntry(new BTreeNode.Entry<>(100, "100"));
+        btree.insertEntry(new BTreeNode.Entry<>(110, "110"));
+        btree.insertEntry(new BTreeNode.Entry<>(120, "120"));
+        btree.insertEntry(new BTreeNode.Entry<>(130, "130"));
+        btree.insertEntry(new BTreeNode.Entry<>(140, "140"));
+        btree.insertEntry(new BTreeNode.Entry<>(150, "150"));
+        btree.insertEntry(new BTreeNode.Entry<>(160, "160"));
+        btree.insertEntry(new BTreeNode.Entry<>(170, "170"));
+        //btree.deleteEntryByKey(10);
         BTreeNode<String> node = btree.locateNodeByKey(10);
         System.out.println(node.toString());
     }

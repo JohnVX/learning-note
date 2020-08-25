eb0038a3ec2d6c81c8cc93ed3ba0caf3be149b6d
JohnVX <wswbpf@163.com>
2020-08-25 11:21:56 +0800

    Lua

--- Lua/lua-note.lua
+++ Lua/lua-note.lua
@@ -78,4 +78,95 @@ function(key,val)--匿名函数
 end
 );
 
+print("Lua 中的变量全是全局变量，那怕是语句块或是函数里，除非用 local 显式声明为局部变量")
+
+i = 1;
+while(i < 3)
+do 
+    print("while loop")
+    i = i+1
+end
+
+for var=1,3 do
+    print("for loop " .. var)
+end
+
+--[ 变量定义 --]
+a = 10
+----[ 执行循环 --]
+repeat
+   print("repeat a的值为:", a)
+   a = a + 1
+until( a > 15 )
+
+s, e = string.find("www.runoob.com", "runoob")
+print("函数可以返回多个结果值，比如string.find，其返回匹配串\"开始和结束的下标\"(如果不存在匹配串返回nil)")
+print(s .. " " .. e)
+
+function maximum (a)
+    local mi = 1             -- 最大值索引
+    local m = a[mi]          -- 最大值
+    for i,val in ipairs(a) do
+       if val > m then
+           mi = i
+           m = val
+       end
+    end
+    return m, mi
+end
+
+print("在return后列出要返回的值的列表即可返回多值")
+print(maximum({8,10,23,12,5}))
+
+function add(...)  
+local s = 0  
+  for i, v in ipairs{...} do   
+    s = s + v  
+  end  
+  return s  
+end  
+print(add(3,4,5,6,7))  --->25
+
+function average(...)
+   result = 0
+   local arg={...}    --> arg 为一个表，局部变量
+   for i,v in ipairs(arg) do
+      result = result + v
+   end
+   print("总共传入 " .. #arg .. " 个数")
+   return result/#arg
+end
+print("平均值为",average(10,5,3,4,5,6))
+
+do  
+    function foo(...)  
+        for i = 1, select('#', ...) do  -->获取参数总数
+            local arg = select(i, ...) -->读取参数
+            print("arg " .. arg)  
+        end  
+    end  
+ 
+    foo(1, 2, 3, 4)  
+end
+
+print([==[
+^
+not    - (unary)
+*      /
++      -
+..
+<      >      <=     >=     ~=     ==
+and
+or
+]==])
+print("除了 ^ 和 .. 外所有的二元运算符都是左连接的")
+
+string1 = "Lua"
+print("\"字符串 1 是\"",string1)
+string2 = 'runoob.com'
+print("字符串 2 是",string2)
+
+string3 = [["Lua 教程"]]
+print("字符串 3 是",string3)
+
 

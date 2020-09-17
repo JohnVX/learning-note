3e1f15893160f494b5b801692da59197fa867168
JohnVX <wswbpf@163.com>
2020-09-17 11:34:21 +0800

    跳表的添加节点实现

--- Algo/list/RunSkipList.java
+++ Algo/list/RunSkipList.java
@@ -5,12 +5,18 @@ import static java.lang.System.out;
 public class RunSkipList {
     public static void main(String[] args) {
         SkipList<String> skipList = new SkipList<>();
+        skipList.add(1, "1");
+        skipList.add(10, "10");
+        skipList.add(5, "5");
         skipList.add(30, "30");
+        skipList.add(70, "70");
         skipList.add(40, "40");
         skipList.add(50, "50");
         skipList.add(60, "60");
-        skipList.add(70, "70");
         skipList.add(90, "90");
+        skipList.add(100, "100");
+        skipList.add(200, "200");
+        skipList.add(150, "150");
         skipList.delete(60);
         out.println(skipList.get(50));
     }
--- Algo/list/SkipList.java
+++ Algo/list/SkipList.java
@@ -1,12 +1,22 @@
 package list;
 
+import java.util.Random;
+
 /**
  * 概率性数据结构
  * 在计算机科学中，跳跃列表是一种数据结构
  * 它使得包含 n 个元素的有序序列的查找和插入操作的平均时间复杂度都是 O(log n)，优于数组的 O(n) 复杂度
  */
 public class SkipList<T>{
-    private Node<T>[] head, tail;
+    private Object[] head;
+    private Random random;
+    public SkipList() {
+        //head 的初始化
+        head = new Object[1];
+        head[0] = null;
+
+        random = new Random(System.currentTimeMillis());
+    }
     static class Node<T>{
         int key;
         T value;
@@ -26,8 +36,9 @@ public class SkipList<T>{
      * @param key key
      * @return T
      */
+    @SuppressWarnings("unchecked")
     public T get(int key){
-        Node<T> node = head[head.length-1].next;
+        Node<T> node = (Node<T>)head[head.length-1];
         Node<T> prev = null;
 
         while (node != null && node.key != key){
@@ -51,16 +62,66 @@ public class SkipList<T>{
     }
 
     /**
-     * 从 head 数组的底层开始, 查找被添加节点应在的位置, 本层添加好节点后, 概率性地选择是否往上层添加节点,
+     * 从 head 数组的底层开始( index == 0 ), 查找被添加节点应在的位置, 本层添加好节点后, 概率性地选择是否往上层添加节点,
      * 若是, 则往上层添加节点, 并重复概率选择操作, 若否，则操作结束
-     * 到达 head 数组的顶层后, 决定是否扩展 head 数组, 若是, 则扩展之, 若否, 则操作结束
+     * 进入 head 数组上层前, 决定是否扩展 head 数组, 若是, 则扩展之
      * @param key key
      * @param value value
      */
+    @SuppressWarnings("unchecked")
     public void add(int key, T value){
-
+        int index = 0;
+        Node<T> nextLevelNode = null;
+        while (true) {
+            if(head.length < (index+1)){
+                extendHeadArray();
+            }
+            Node<T> node = (Node<T>)head[index];
+            if (node == null) {
+                if(index > 0){
+                    Node<T> copiedNode = (Node<T>)head[index-1];
+                    if(copiedNode.key != key){
+                        //构建上层的第一个数据节点
+                        head[index] = new Node<>(copiedNode.key, copiedNode.value, null, copiedNode);
+                        node = (Node<T>)head[index];
+                    }else {
+                        //添加第一个节点
+                        head[index] = new Node<>(key, value, null, copiedNode);
+                        return;
+                    }
+                }else {
+                    //添加第一个节点
+                    head[index] = new Node<>(key, value, null, null);
+                    return;
+                }
+            }
+            Node<T> prev = (Node<T>)head[index];
+            for (; node != null && node.key < key; prev = node, node = node.next) ;
+            //node == null or node.key >= key
+            prev.next = new Node<>(key, value, node, nextLevelNode);
+            //0.5 的命中概率, 概率性地选择是否往上层添加节点
+            if (random.nextInt(10000) < 5000) {
+                index++;
+                nextLevelNode = prev.next;
+            } else {
+                return;
+            }
+        }
     }
+
+    /**
+     * 从 head 数组的底层开始, 查找要被删除的节点, 找到后删除之,
+     * 依次向上进入 head 数组的上层, 删除对应的节点,
+     * 删除节点后, 若 head 数组的最上层( index == (length-1) )已无数据节点, 则重建 head 数组以去掉数组最上层
+     * @param key key
+     */
     public void delete(int key){
 
+    }
+    private void extendHeadArray(){
+        int newLength = head.length * 2;
+        Object[] newHead = new Object[newLength];
+        System.arraycopy(head, 0, newHead, 0, head.length);
+        head = newHead;
     }
 }

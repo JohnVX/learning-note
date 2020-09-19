e16d3c4359e635b9a4a66857877e39c0101a12fb
JohnVX <wswbpf@163.com>
2020-09-19 19:02:29 +0800

    调试调表实现

--- Algo/list/SkipList.java
+++ Algo/list/SkipList.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ThreadLocalRandom;
 /**
  * 概率性数据结构
  * 在计算机科学中，跳跃列表是一种数据结构
- * 它使得包含 n 个元素的有序序列的查找和插入操作的平均时间复杂度都是 O(log n)，优于数组的 O(n) 复杂度
+ * 它使得包含 n 个元素的有序序列的查找、插入、删除操作的平均时间复杂度都是 O(log n)，优于数组的 O(n) 复杂度
  */
 public class SkipList<T>{
     private Node[] head;

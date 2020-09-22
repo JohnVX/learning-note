a376cfd48fbd10b3e6e5701bdc39b5dabe34f69c
JohnVX <wswbpf@163.com>
2020-09-22 17:31:41 +0800

    图论中最短路径的弗洛伊德算法、贝尔曼福特算法

---
+++ Algo/graph/BellmanFord.java
@@ -0,0 +1,9 @@
+package graph;
+
+/**
+ * 求解单源最短路径问题的一种算法
+ * 其优于 Dijkstra 算法的方面是边的权值可以为负数、实现简单，缺点是时间复杂度过高
+ */
+public class BellmanFord {
+
+}
---
+++ Algo/graph/FloydWarshall.java
@@ -0,0 +1,10 @@
+package graph;
+
+/**
+ * 任意两点间的最短路径
+ * 可以正确处理有向图或负权，但不可存在负权回路
+ *
+ */
+public class FloydWarshall {
+
+}
